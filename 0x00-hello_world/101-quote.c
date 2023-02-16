404a732a9d8997ef9637748cf5145f1b9606a7fb
simrett <leesim242@gmail.com>
2023-02-16 12:18:40 -0800

    lasttt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
 
 /**
-*main - Entry point
-*Return: Always 1
-*/
-
+ * main - Entry point of the program
+ *
+ * Return: 1 (Error)
+ */
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-                return (1);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = 0;
+	while (msg[len] != '\0')
+	{
+		len++;
+	}
+
+	write(2, msg, len);
+	return (1);
 }
