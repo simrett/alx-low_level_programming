4a7d4bd5ebadacbff9fea74ce3cf4504e2d5b565
simrett <leesim242@gmail.com>
2023-02-17 21:33:15 -0800

    eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(48 + n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(48 + n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
