eaa5f78f562fa4254752caf72d482373d165ec7f
simrett <leesim242@gmail.com>
2023-02-19 15:59:12 -0800

    ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,23 @@
  */
 int main(void)
 {
-	int num1 = 0, num2;
+	int a, b;
 
-	for (num2 = num1 + 1; num2 < 10; num1++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (num2 = num1 + 1; num2 < 10; num2++)
-		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
-			if (num1 != 8 || num2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (b = a + 1; b <= 99; b++)
+	{
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+	putchar(' ');
+	putchar((b / 10) + '0');
+	putchar((b % 10) + '0');
+	if (a == 98 && b == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
-
 	return (0);
 }
