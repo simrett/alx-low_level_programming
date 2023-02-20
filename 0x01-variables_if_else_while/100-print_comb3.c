b347aa7d8aeddbef1d3d0f4cac03199ab760667d
simrett <leesim242@gmail.com>
2023-02-19 17:28:22 -0800

    answer

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
+/**
+ * main - Prints all single numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 100; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            if (i != j)
-            {
-                if (i == 0 && j == 1)
-                    continue;
-                if (i != 0)
-                    putchar(i / 10 + '0');
-                putchar(i % 10 + '0');
-                putchar(',');
-                putchar(' ');
-                putchar(j / 10 + '0');
-                putchar(j % 10 + '0');
-                if (i == 98 && j == 99)
-                    continue;
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				 putchar(',');
+				 putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
