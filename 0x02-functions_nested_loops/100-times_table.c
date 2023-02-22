642c09461ca0bd95eb0ebbe39d8f1f0f0274ba46
simrett <leesim242@gmail.com>
2023-02-21 17:55:00 -0800

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,7 @@
 /**
  * print_times_table - times table for n
  *
+ *
  * @n: value of table to be printed starting from 0
  *
  * Return: void
@@ -11,6 +12,7 @@
 
 void print_times_table(int n)
 {
+
 	int num, multi, prod;
 
 	if (n >= 0 && n <= 15)
@@ -18,32 +20,32 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 
+	{
+		_putchar('0');
+		for (multi = 1; multi <= n; multi++)
+
 		{
-			_putchar('0');
-			for (multi = 1; multi <= n; multi++)
+			_putchar(',');
+			_putchar(' ');
+			prod = num * multi;
+			if (prod <= 99)
+				_putchar(' ');
+			if (prod <= 9)
+				_putchar(' ');
+			if (prod >= 100)
 
 			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * multi;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10) % 10 + '0');
+			}
 
-				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-				}
+		else if (prod <= 99 && prod >= 10)
 
-				else if (prod <= 99 && prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+			}
 
-				{
-					_putchar((prod / 10) + '0');
-				}
-
-				_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
