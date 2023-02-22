81e6b0ada0a97d2244676fa9b759c74e0963a44d
simrett <leesim242@gmail.com>
2023-02-21 18:34:46 -0800

    nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	int a, b, c, d, e;
 	for (a = 0; a <= 9; a++)
 
-       		{
+		{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c > 9)	
+			if (c > 9)
 			{
 				d = c % 10;
 				e = (c - d) / 10;
