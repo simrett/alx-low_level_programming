6ae836c5a75bc5b3511ef0f1bfc560bdae2ba893
simrett <leesim242@gmail.com>
2023-02-21 18:31:22 -0800

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_times_table - Prints the times table of the input,starting with 0. 
+ * print_times_table - Prints the times table of the input,starting with 0.
  *
  * @n: value of table to be printed starting from 0
  *
