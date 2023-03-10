af57831bd8b83e52533f25cfd30dd66b44c1f424
simrett <leesim242@gmail.com>
2023-03-10 08:56:09 -0800

    _putchar.c

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - writes a character to stdout
+ *
+ * @c: the character to write
+ *
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
