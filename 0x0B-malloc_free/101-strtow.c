af7c59298c9e274c9fe372096e52e58662849870
simrett <leesim242@gmail.com>
2023-03-13 07:59:38 -0700

    #include <stdlib.h>

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
+*/
+
+int count_word(char *s)
+
+{
+int con, x, y;
+
+con = 0;
+y = 0;
+
+for (x = 0; s[x] != '\0'; x++)
+{
+if (s[x] == ' ')
+con = 0;
+else if (con == 0)
+{
+con = 1;
+y++;
+}
+}
+return (y);
+}
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+
+char **strtow(char *str)
+
+{
+char **box, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
+
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
+box = (char **) malloc(sizeof(char *) * (words + 1));
+if (box == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+box[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
+box[k] = NULL;
+return (box);
+}
