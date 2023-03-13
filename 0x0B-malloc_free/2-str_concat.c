3fe6474dabdf9a22e79f57159226d3f110774d7c
simrett <leesim242@gmail.com>
2023-03-13 07:45:50 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,19 +1,63 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+unsigned int _strlen(char *str);
+
 /**
- * main - Prints the name of arguments passed to it
- * @argc: counts of arguements on command line
- * @argv: pointer to the array of command line arguements
- * Return: Always 0
+ * str_concat - creates a memory space where it concatenates two strings
+ * @s1: string one to be concatenated with string s2
+ * @s2: string two to be concatenated with string s1
+ *
+ * Return: the pointer to the newly allocated memory space, NULL if failure
  */
-
-int main(int argc, char *argv[])
+char *str_concat(char *s1, char *s2)
 {
-int i;
+unsigned int i, j, len_s1, len_s2;
+char *ptr;
 
-for (i = 0; i < argc; i++)
+len_s1 = _strlen(s1);
+len_s2 = _strlen(s2);
+
+ptr = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+if (ptr == NULL)
 {
-printf("%s\n", argv[i]);
+return (NULL);
 }
+
+for (i = 0, j = 0; j < len_s1; i++, j++)
+{
+ptr[i] = s1[j];
+}
+
+for (j = 0; j < len_s2; i++, j++)
+{
+ptr[i] = s2[j];
+}
+
+ptr[i] = '\0';
+
+return (ptr);
+}
+
+/**
+ * _strlen - calculates the length of string str
+ * @str: string that the length will be calculated
+ *
+ * Return: the length of string str
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+
+if (str == NULL)
+{
 return (0);
+}
+
+for (i = 0; str[i] != '\0'; i++)
+;
+
+
+return (i);
 }
