0bb140f1dce6b295ff80f0ad3a18b82c74759ac6
simrett <leesim242@gmail.com>
2023-03-20 14:47:50 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,23 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
 
-#define PI 3.14159265359
+/**
+ * init_dog - initializes variable d of type struct dog
+ * @d: variable to be initialized
+ * @name: name of dog in variable d
+ * @age: age of dog in variable d
+ * @owner: name of dog's owner in variable d
+ *
+ * Return: void
+ */
 
-#endif /* ifndef OBJECT_LIKE_MACRO_H */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
