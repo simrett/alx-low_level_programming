648ae05c8f570c0f5fab302b9091fa42552d1955
simrett <leesim242@gmail.com>
2023-04-03 12:54:25 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
+	int i = 0;
 
-while (h != NULL)
-{
-printf("%i\n", h->n);
-h = h->next;
-i++;
-}
-return (i);
+	while (h != NULL)
+	{
+	printf("%i\n", h->n);
+			h = h->next;
+			i++;
+	}
+	return (i);
 }
