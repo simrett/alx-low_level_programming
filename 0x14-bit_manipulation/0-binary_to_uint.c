b0d78edf2452433640f84850132d04a5fc21b0af
simrett <leesim242@gmail.com>
2023-04-06 12:26:34 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,21 +5,21 @@
  *
  * @b: pointer to a string of 0 and 1 characters
  *
- * Return: The coverted number or c0 if the string contains non-0/1 chars
+ * Return: The coverted number or 0 if the string contains non-0/1 chars
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int bit = 0;
+	unsigned int bit = 0;
 
-while (b && *b)
-{
-if (*b != '0' && *b != '1')
-return (0);
+	while (b && *b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
 
-bit = bit << 1;
-bit = bit | (*b - '0');
-b++;
-}
-return (bit);
+		bit = bit << 1;
+		bit = bit | (*b - '0');
+		b++;
+	}
+	return (bit);
 }
