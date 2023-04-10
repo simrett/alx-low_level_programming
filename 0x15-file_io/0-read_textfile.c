1a721eb0c7273e0189e34943e261907a8760b9af
simrett <leesim242@gmail.com>
2023-04-10 15:43:14 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,50 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * read_textfile - reads a tect file and prints it to the POSIX std-output.
+ * read_textfile - reads a text file and prints it to the POSIX std-output.
  * @filename: the file to open.
  * @letters: number of letters it should read and print.
- * Return: numbers of letters it could read and print, or 0 if:
+ * Return: number of letters it could read and print, or 0 if:
  * - filename is NULL.
- * - the file cannot be open or read.
+ * - the file can not be opened or read.
  * - write fails or does not write the expected amount of bytes.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_read, bytes_written;
-	int fd;
-	char *buffer;
+int fd = -1;
+ssize_t output = 0;
+char *ch;
 
-	if (filename == NULL)
-		return (0);
+if (!filename)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+ch = malloc(sizeof(char) * letters);
+if (!ch)
+{
+close(fd);
+return (0);
+}
 
-	buffer[bytes_read] = '\0';
+output = read(fd, ch, letters);
+if (output < 0)
+{
+free(ch);
+close(fd);
+return (0);
+}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);
-	close(fd);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		return (0);
-	return (bytes_written);
+output = write(STDOUT_FILENO, ch, output);
+free(ch);
+close(fd);
+if (output < 0)
+return (0);
+return (output);
 }
