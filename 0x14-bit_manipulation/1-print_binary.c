0360514f90f5bdd5db61f2a6915042a3e1e8eb67
simrett <leesim242@gmail.com>
2023-05-04 09:17:15 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,6 +5,7 @@
  * @number: the number to print in binary.
  *
  * Return: none.
+ *
  */
 void print_binary(unsigned long int number)
 {
@@ -13,12 +14,10 @@ void print_binary(unsigned long int number)
 		if (number >> 1)
 			print_binary(number >> 1);
 
-		/* print the least significant bit */
 		_putchar((number & 1) + '0');
 	}
 	else
 	{
-		/* number is zero */
 		_putchar('0');
 	}
 }
