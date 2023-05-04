eddba3020d0776a2a3e55c3dcfa0e076113ad449
zakisu250 <zakisu250@gmail.com>
2023-05-04 12:19:46 -0300

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,14 +12,14 @@ unsigned int count_bits_to_flip(unsigned long int num1, unsigned long int num2)
 {
 	unsigned int bits_to_flip = 0;
 
-	while (num1 != 0 || num2 != 0) {
-		if ((num1 & 1) != (num2 & 1)) {
+	while (num1 != 0 || num2 != 0)
+	{
+		if ((num1 & 1) != (num2 & 1))
 			bits_to_flip++;
-		}
 
 		num1 >>= 1;
 		num2 >>= 1;
 	}
 
-	
+	return (bits_to_flip);
 }
