d31a2c9fdc993651f26fd1abf9796a0d85245657
simrett <leesim242@gmail.com>
2023-05-08 16:02:22 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,36 +15,36 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = -1;
-	ssize_t output = 0;
-	char *ch;
+int fd = -1;
+ssize_t output = 0;
+char *ch;
 
-	if (!filename)
-		return (0);
+if (!filename)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
 
-	ch = malloc(sizeof(char) * letters);
-	if (!ch)
-	{
-		close(fd);
-			return (0);
-	}
+ch = malloc(sizeof(char) * letters);
+if (!ch)
+{
+close(fd);
+return (0);
+}
 
-	output = read(fd, ch, letters);
-	if (output < 0)
-	{
-		free(ch);
-		close(fd);
-			return (0);
-	}
+output = read(fd, ch, letters);
+if (output < 0)
+{
+free(ch);
+close(fd);
+return (0);
+}
 
-	output = write(STDOUT_FILENO, ch, output);
-	free(ch);
-	close(fd);
-	if (output < 0)
-		return (0);
-	return (output);
+output = write(STDOUT_FILENO, ch, output);
+free(ch);
+close(fd);
+if (output < 0)
+return (0);
+return (output);
 }
