6457f28aa157ee29214df2c4381aff0a2160347c
simrett <leesim242@gmail.com>
2023-06-15 16:13:09 -0700

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,54 @@
 #include "lists.h"
 
 /**
- * dlistint_t_len - Return the length of a dlistint_t list
- * @h: head for node
- * Return: the length of the list
- */
-size_t dlistint_t_len(const dlistint_t *h)
-{
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
- * insert_dnodeint_at_index - Inserts a new node at given position
- * @h: struct node
- * @idx: index of node to add
- * @n: node data
- * Return: Address of new node or NULL if it failed
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *curr;
+	dlistint_t *new;
+	dlistint_t *head;
 	unsigned int i;
 
-	if (idx > dlistint_t_len(*h))
-		return (NULL);
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->prev = NULL;
-	new->n = n;
-	new->next = NULL;
-
-	curr = *h;
-	for (i = 0; i < (idx - 1); i++)
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		curr = curr->next;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	new->prev = curr;
-	new->next = curr->next;
-	curr->next->prev = new;
-	curr->next = new;
+
 	return (new);
 }
