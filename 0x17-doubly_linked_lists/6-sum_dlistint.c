0f16f596e7aee93e7ee0caf2a09b643ffd40ba4f
simrett <leesim242@gmail.com>
2023-06-15 15:30:58 -0700

    returns the sum of all the data

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,57 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the length of a dlistint_t list.
- * @h: Pointer to the head of the list.
- * Return: The length of the list.
+ * sum_dlistint - Return the sum of node data
+ * @head: struct node
+ * Return: the sum of node values or 0 if empty
  */
-size_t dlistint_len(const dlistint_t *h)
+int sum_dlistint(dlistint_t *head)
 {
-	size_t count = 0;
+	dlistint_t *tmp = head;
+	unsigned int sum = 0;
 
-	while (h != NULL)
+	while (tmp != NULL)
 	{
-		count++;
-		h = h->next;
+		sum += tmp->n;
+		tmp = tmp->next;
 	}
-
-	return (count);
-}
-
-/**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Double pointer to the head of the list.
- * @idx: Index of the node to add.
- * @n: Node data.
- * Return: Address of the new node or NULL if it fails.
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-	dlistint_t *new_node, *current;
-	unsigned int i;
-
-	if (idx > dlistint_len(*h))
-		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->prev = NULL;
-	new_node->n = n;
-	new_node->next = NULL;
-
-	current = *h;
-	for (i = 0; i < idx - 1; i++)
-	{
-		current = current->next;
-	}
-
-	new_node->prev = current;
-	new_node->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	current->next = new_node;
-
-	return (new_node);
+	return (sum);
 }
