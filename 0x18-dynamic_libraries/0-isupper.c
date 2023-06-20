4f7e260bbff6be6c1041ef920e16e9898dfc9031
simrett <leesim242@gmail.com>
2023-06-19 17:45:11 -0700

    first task

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case letter
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if the letter is uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c <= 'Z' && c >= 'A')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
